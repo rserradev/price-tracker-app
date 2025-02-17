6d9e1d8d9ba1ecfd1346c6756ea37e651e6fe0ac
rserradev <ronaldoserra1998@gmail.com>
2025-02-16 21:36:32 -0300

    testing with paris cl

--- price_tracker/scraper/scraping_test.py
+++ price_tracker/scraper/scraping_test.py
@@ -1,7 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 # URL de la página de productos
-URL = "https://quotes.toscrape.com/"
+URL = "https://www.paris.cl/zapatillas-urbana-cuero-knu-skool-unisex-614258.html"
 
 # Simulación de un navegador web
 HEADERS = {"User-Agent": "Mozilla/5.0"}
@@ -9,4 +11,17 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 # Hacer la petición GET a la URL
 response = requests.get(URL, headers=HEADERS)
 
-print("Scraping...")
+# Verificar si la petición fue exitosa
+if response.status_code == 200:
+    print("Petición exitosa")
+
+    # Analizar el html de la respuesta
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Obtener el nombre y precio del producto
+    name = soup.find("div", {"class": "pdp-top__product-name"}).text
+    price = soup.find("div", {"class": "bottom-content-prices__container"}).text
+
+    print(f"Nombre: {name} Precio: {price}")
+else:
+    print(f"Petición fallida: {response.status_code}")
