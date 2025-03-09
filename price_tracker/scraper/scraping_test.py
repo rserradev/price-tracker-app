b3a9e1eb6f9ba373258351331a3070da9149ef0b
rserradev <ronaldoserra1998@gmail.com>
2025-03-09 20:38:09 -0300

    feat: extracion de precio especifico

--- price_tracker/scraper/scraping_test.py
+++ price_tracker/scraper/scraping_test.py
@@ -1,27 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_html(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Error al obtener, el código de estado es {response.status_code}")
+        return None
 
-# URL de la página de productos
-URL = "https://www.paris.cl/zapatillas-urbana-cuero-knu-skool-unisex-614258.html"
+def get_content_by_class_and_testid(html):
+    soup = BeautifulSoup(html, "html.parser")
+    # Buscar todos los elementos <span> con el data-testid="paris-text"
+    tags = soup.find_all("span", {"data-testid": "paris-text"})
+    if tags:
+        for tag in tags:
+            # Verificar si el text contiene el signo $
+            if "$" in tag.text:
+                price = tag.text.strip()
+                print(f'El precio es {price}')
+                break
+    else:
+        print("No se encontraron elementos con el data-testid especificado")
 
-# Simulación de un navegador web
-HEADERS = {"User-Agent": "Mozilla/5.0"}
-
-# Hacer la petición GET a la URL
-response = requests.get(URL, headers=HEADERS)
-
-# Verificar si la petición fue exitosa
-if response.status_code == 200:
-    print("Petición exitosa")
-
-    # Analizar el html de la respuesta
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # Obtener el nombre y precio del producto
-    name = soup.find("div", {"class": "pdp-top__product-name"}).text
-    price = soup.find("div", {"class": "bottom-content-prices__container"}).text
-
-    print(f"Nombre: {name} Precio: {price}")
+html = get_html("https://nuevo.paris.cl/zapatillas-urbana-cuero-knu-skool-unisex-614258.html")
+if html:
+    get_content_by_class_and_testid(html)
 else:
-    print(f"Petición fallida: {response.status_code}")
+    print("No se pudo obtener el HTML")
